b8cbd73547d4a07b2b9b0814ed448b15cea5e39a
birkin <birkin.diana@gmail.com>
2018-04-09 16:35:54 -0400

    stubs module.

---
+++ lib/sierra.py
@@ -0,0 +1,10 @@
+import logging, os
+
+logging.basicConfig(
+    filename=os.environ['SBE__LOG_PATH'],
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S'
+    )
+log = logging.getLogger(__name__)
+log.debug( 'loading sierra module' )
