4ae52170618e7220bd5fab203eaddb175ee73529
birkin <birkin.diana@gmail.com>
2018-08-02 10:12:39 -0400

    starts stand-alone validator using pymarc.

--- lib/validator.py
+++ lib/validator.py
@@ -1,6 +1,7 @@
-import glob, logging, ntpath, os, pprint, shutil
+import datetime, glob, logging, ntpath, os, pprint, shutil, sys
+sys.path.append( os.path.abspath(os.getcwd()) )
 from lib.tracker import TrackerHelper
-
+from pymarc import MARCReader
 
 logging.basicConfig(
     filename=os.environ['SBE__LOG_PATH'],
@@ -14,6 +15,8 @@ log = logging.getLogger(__name__)
 log.debug( 'loading validator module' )
 
 tracker_helper = TrackerHelper()
+
+os.nice( 19 )
 
 
 class FileChecker( object ):
@@ -32,27 +35,66 @@ class FileChecker( object ):
         if tracker['files_validated']:
             log.debug( 'files already validated; returning' )
             return
-        marc_file_list = glob.glob( '%s/*.mrc' % self.FILE_DOWNLOAD_DIR )
+        # marc_file_list = glob.glob( '%s/*.mrc' % self.FILE_DOWNLOAD_DIR )
+        marc_file_list = sorted( glob.glob( '%s/*.*' % self.FILE_DOWNLOAD_DIR ) )
+        log.debug( 'marc_file_list, ```%s```' % marc_file_list )
+        start = datetime.datetime.now()
         for file_path in marc_file_list:
-            size_in_bytes = os.path.getsize( file_path )
-            if size_in_bytes < 1000:
-                log.debug( 'file, ```%s``` is only `%s` bytes' % (file_path, size_in_bytes) )
-                self.open_and_check_file( file_path )
+            # size_in_bytes = os.path.getsize( file_path )
+            # if size_in_bytes < 1000:
+            #     log.debug( 'file, ```%s``` is only `%s` bytes' % (file_path, size_in_bytes) )
+            #     self.open_and_check_file( file_path )
+            self.open_and_check_file( file_path )
         tracker_helper.update_validation_status( tracker )
+        time_taken = str( datetime.datetime.now() - start )
+        log.debug( 'time_taken, `%s`' % time_taken )
         return
 
     def open_and_check_file( self, file_path ):
         """ Opens suspicious file.
             Called by validate_marc_files() """
-        with open( file_path, 'rt', encoding='utf8' ) as f:
-            content = f.read()
-            for bad_data_check in self.non_marc_indicators:
-                if bad_data_check in content:
-                    file_name = os.path.basename( file_path )
-                    new_file_name = file_name.replace( '.mrc', '.txt' )
-                    new_file_path = '%s/%s' % ( self.FILE_DOWNLOAD_DIR, new_file_name)
-                    log.debug( 'moving bad-file to new_file_path, ```%s```' % new_file_path )
-                    shutil.move( file_path, new_file_path )
+        with open( file_path, 'rb' ) as fh:
+            log.debug( 'file about to be checked, ```%s```' % file_path )
+            reader = MARCReader( fh )
+            try:
+                for record in reader:
+                    # message = 'file_path, ```%s``` is good; title, ```%s```' % ( file_path, record.title() )
+                    # print( message )
+                    pass
+                log.debug( 'file ok' )
+            except Exception as e:
+                log.error( 'exception, `%s`' % e )
+        # with open( file_path, 'rt', encoding='utf8' ) as f:
+        #     content = f.read()
+        #     for bad_data_check in self.non_marc_indicators:
+        #         if bad_data_check in content:
+        #             file_name = os.path.basename( file_path )
+        #             new_file_name = file_name.replace( '.mrc', '.txt' )
+        #             new_file_path = '%s/%s' % ( self.FILE_DOWNLOAD_DIR, new_file_name)
+        #             log.debug( 'moving bad-file to new_file_path, ```%s```' % new_file_path )
+        #             shutil.move( file_path, new_file_path )
         return
 
+    # def open_and_check_file( self, file_path ):
+    #     """ Opens suspicious file.
+    #         Called by validate_marc_files() """
+    #     with open( file_path, 'rt', encoding='utf8' ) as f:
+    #         content = f.read()
+    #         for bad_data_check in self.non_marc_indicators:
+    #             if bad_data_check in content:
+    #                 file_name = os.path.basename( file_path )
+    #                 new_file_name = file_name.replace( '.mrc', '.txt' )
+    #                 new_file_path = '%s/%s' % ( self.FILE_DOWNLOAD_DIR, new_file_name)
+    #                 log.debug( 'moving bad-file to new_file_path, ```%s```' % new_file_path )
+    #                 shutil.move( file_path, new_file_path )
+    #     return
+
     ## end class FileChecker()
+
+
+if __name__ == '__main__':
+    log.debug( 'starting' )
+    tracker = { 'files_validated': False }  # assumes a direct call to the validator should really validate the files
+    checker = FileChecker()
+    checker.validate_marc_files( tracker )
+    log.debug( 'complete' )
