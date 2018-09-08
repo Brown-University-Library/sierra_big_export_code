a21fbf229790f178c2a8f61547e01cc2029d8245
birkin <birkin.diana@gmail.com>
2018-09-08 09:57:11 -0400

    handles no-records in test-script.

--- lib/misc.py
+++ lib/misc.py
@@ -24,30 +24,62 @@ class Tester( object ):
         self.HTTPBASIC_SECRET = os.environ['SBE__HTTPBASIC_PASSWORD']
         self.FILE_DOWNLOAD_DIR = os.environ['SBE__FILE_DOWNLOAD_DIR']
 
+    # def manage_download( self ):
+    #     """ Shot-maker.
+    #         Called by if/main() """
+    #     bib_range = ( 2160000, 2162000 )
+    #     try:
+    #         token = self.get_token()
+    #         file_url = self.make_bibrange_request( token, bib_range )
+    #         if file_url:
+    #             file_name = '%s_file.mrc' % str( datetime.datetime.now() ).replace( ' ', 'T' )
+    #             self.grab_file( token, file_url, file_name )
+    #     except Exception as e:
+    #         log.error( 'initial exception, ```%s```' % str(e) )
+    #         time.sleep( 5 )
+    #         log.debug( '\nslept 5 seconds' )
+    #         token = self.get_token()
+    #         file_url = self.make_bibrange_request( token, bib_range )
+    #         if file_url:
+    #             file_name = '%s_file.mrc' % str( datetime.datetime.now() ).replace( ' ', 'T' )
+    #             self.grab_file( token, file_url, file_name )
+    #     return
+
     def manage_download( self ):
         """ Shot-maker.
             Called by if/main() """
-        bib_range = ( 1246000, 1248000 )
+        bib_range = ( 2160000, 2162000 )
         try:
             token = self.get_token()
-            file_url = self.make_bibrange_request( token, bib_range )
+            ( file_url, err ) = self.make_bibrange_request( token, bib_range )
+            try:
+                bibrange_response_dct = json.loads( err )
+                if bibrange_response_dct.get( 'outputRecords' ) == 0:
+                    file_name = '%s_file.mrc' % str( datetime.datetime.now() ).replace( ' ', 'T' )
+                    self.save_file( err, file_name)
+            except Exception as e:
+                message = 'problem reading error as json, ```%s``; raising Exception'
+                log.error( message )
+                raise Exception( message )
             if file_url:
                 file_name = '%s_file.mrc' % str( datetime.datetime.now() ).replace( ' ', 'T' )
                 self.grab_file( token, file_url, file_name )
         except Exception as e:
-            log.error( 'initial exception, ```%s```' % str(e) )
-            time.sleep( 5 )
-            log.debug( '\nslept 5 seconds' )
-            token = self.get_token()
-            file_url = self.make_bibrange_request( token, bib_range )
-            if file_url:
-                file_name = '%s_file.mrc' % str( datetime.datetime.now() ).replace( ' ', 'T' )
-                self.grab_file( token, file_url, file_name )
+            message = 'exeption, ```%s```' % e
+            log.error( message )
+            raise Exception( message )
+            # time.sleep( 5 )
+            # log.debug( '\nslept 5 seconds' )
+            # token = self.get_token()
+            # file_url = self.make_bibrange_request( token, bib_range )
+            # if file_url:
+            #     file_name = '%s_file.mrc' % str( datetime.datetime.now() ).replace( ' ', 'T' )
+            #     self.grab_file( token, file_url, file_name )
         return
 
     def get_token( self ):
         """ Gets API token.
-            Called by controller.download_file() """
+            Called by manage_download() """
         token = 'init'
         token_url = '%stoken' % self.API_ROOT_URL
         log.debug( 'token_url, ```%s```' % token_url )
@@ -62,25 +94,23 @@ class Tester( object ):
             raise Exception( message )
         return token
 
-    # def initiate_bibrange_request( self, token, next_batch ):
-    #     """ Makes request that returns the marc file url.
-    #         Called by controller.download_file() """
-    #     try:
-    #         file_url = self.make_bibrange_request( token, next_batch )
-    #         return file_url
-    #     except Exception as e:
-    #         log.error( 'exception, ```%s```' % e )
-    #         time.sleep( 10 )
-    #         try:
-    #             file_url = self.make_bibrange_request( token, next_batch )
-    #             return file_url
-    #         except Exception as e:
-    #             log.error( '2nd exception, ```%s```; quitting' % e )
-    #             sys.exit()
+    # def make_bibrange_request( self, token, bib_range ):
+    #     """ Forms and executes the bib-range query.
+    #         Called by manage_download() """
+    #     start_bib = bib_range[0]
+    #     end_bib = bib_range[1]
+    #     marc_url = '%sbibs/marc' % self.API_ROOT_URL
+    #     payload = { 'id': '[%s,%s]' % (start_bib, end_bib), 'limit': (end_bib - start_bib) + 1, 'mapping': 'toc' }
+    #     log.debug( 'payload, ```%s```' % payload )
+    #     custom_headers = { 'Authorization': 'Bearer %s' % token }
+    #     r = requests.get( marc_url, headers=custom_headers, params=payload, timeout=30 )
+    #     file_url = self.assess_bibrange_response( r )
+    #     log.debug( 'returning file_url, ```%s```' % file_url )
+    #     return file_url
 
     def make_bibrange_request( self, token, bib_range ):
         """ Forms and executes the bib-range query.
-            Called by initiate_bibrange_request() """
+            Called by manage_download() """
         start_bib = bib_range[0]
         end_bib = bib_range[1]
         marc_url = '%sbibs/marc' % self.API_ROOT_URL
@@ -88,23 +118,32 @@ class Tester( object ):
         log.debug( 'payload, ```%s```' % payload )
         custom_headers = { 'Authorization': 'Bearer %s' % token }
         r = requests.get( marc_url, headers=custom_headers, params=payload, timeout=30 )
-        file_url = self.assess_bibrange_response( r )
+        ( file_url, err ) = self.assess_bibrange_response( r )
         log.debug( 'returning file_url, ```%s```' % file_url )
-        return file_url
+        log.debug( 'returning err, ```%s```' % err )
+        return ( file_url, err )
 
     def assess_bibrange_response( self, r ):
         """ Analyzes bib-range response.
             Called by make_bibrange_request() """
         log.debug( 'r.status_code, `%s`' % r.status_code )
         log.debug( 'bib r.content, ```%s```' % r.content )
+        file_url = err = None
         if r.status_code is not 200:
             message = 'bad status code; raising Exception'
             log.error( message )
             raise Exception( message )
+
+        if r.json().get( 'outputRecords', None ) == 0:
+            log.info( 'no records found for this bib-range, returning bib-range-response' )
+            err = r.content
+            return ( file_url, err )
+
         file_url = r.json().get( 'file', None )
         if file_url:
             log.debug( 'normal file_url found, it is ```%s```' % file_url )
-            return file_url
+            return ( file_url, err )
+
         if r.json().get( 'name', None ) == 'Rate exceeded for endpoint':
             message = 'problem: ```Rate exceeded for endpoint; raising Exception```'
             log.error( message )
@@ -116,11 +155,36 @@ class Tester( object ):
         message = 'problem, no file-url or known problem -- check r.content and handle; raising Exception'
         log.error( message )
         raise Exception( message )
-        return
+
+    # def assess_bibrange_response( self, r ):
+    #     """ Analyzes bib-range response.
+    #         Called by make_bibrange_request() """
+    #     log.debug( 'r.status_code, `%s`' % r.status_code )
+    #     log.debug( 'bib r.content, ```%s```' % r.content )
+    #     if r.status_code is not 200:
+    #         message = 'bad status code; raising Exception'
+    #         log.error( message )
+    #         raise Exception( message )
+    #     file_url = r.json().get( 'file', None )
+    #     if file_url:
+    #         log.debug( 'normal file_url found, it is ```%s```' % file_url )
+    #         return file_url
+    #     if r.json().get( 'name', None ) == 'Rate exceeded for endpoint':
+    #         message = 'problem: ```Rate exceeded for endpoint; raising Exception```'
+    #         log.error( message )
+    #         raise Exception( message )
+    #     elif r.json().get( 'name', None ) == 'External Process Failed':
+    #         message = 'problem: ```External Process Failed; raising Exception```'
+    #         log.error( message )
+    #         raise Exception( message )
+    #     message = 'problem, no file-url or known problem -- check r.content and handle; raising Exception'
+    #     log.error( message )
+    #     raise Exception( message )
+    #     return
 
     def grab_file( self, token, file_url, file_name ):
         """ Downloads file.
-            Called by controller.download_file() """
+            Called by manage_download() """
         log.debug( 'starting grab_file()' )
         # token = '42'
         custom_headers = {'Authorization': 'Bearer %s' % token }
@@ -136,6 +200,21 @@ class Tester( object ):
             with open(filepath, 'wb') as file_handler:
                 for chunk in r.iter_content( chunk_size=128 ):
                     file_handler.write( chunk )
+                log.debug( 'file written to ```%s```' % filepath )
+        except Exception as e:
+            message = 'exception writing file, ```%s```; raising Exception' % e
+            log.error( message )
+            raise Exception( message )
+        return
+
+    def save_file( self, err_output, file_name ):
+        """ Saves file.
+            Called by manage_download() """
+        filepath = '%s/%s' % ( self.FILE_DOWNLOAD_DIR, file_name )
+        log.debug( 'filepath, ```%s```' % filepath )
+        try:
+            with open(filepath, 'wb') as file_handler:
+                file_handler.write( err_output )
                 log.debug( 'file written to ```%s```' % filepath )
         except Exception as e:
             message = 'exception writing file, ```%s```; raising Exception' % e
